6a4fc5aa53383d909fea799cee7f73e2ce59518b
TehHitman <68774870+TehHitman@users.noreply.github.com>
2025-03-04 16:49:30 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'time2rp mapping team https://discord.gg/time2rp '
+description 'time2rp mapping team '
+version '1.0.0'
+
+this_is_a_map 'yes'
+
